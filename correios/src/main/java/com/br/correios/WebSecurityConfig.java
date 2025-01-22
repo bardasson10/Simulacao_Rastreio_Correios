872842b6be28afde2afbff5061cf9121af67c320
Bardasson <gabriel123.bardasson@gmail.com>
2025-01-22 00:22:57 -0300

    #Refactor: WebSecurity Cors e #Feat: Add Cors Origin

--- correios/src/main/java/com/br/correios/WebSecurityConfig.java
+++ correios/src/main/java/com/br/correios/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/", "http://"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
